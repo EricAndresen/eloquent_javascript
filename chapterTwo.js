60a536c8977d1ad482c5b861882f72917bb8572e
Eric-Andresen <erandre7@gmail.com>
2018-10-05 12:02:06 -0700

    Ex 2.1

---
+++ chapterTwo.js
@@ -0,0 +1,8 @@
+// Exercise 1 | Write a function min that takes two args and returns their minimum
+const min = (num1, num2) => (num1 <= num2) ? num1 : num2;
+
+// Tests
+// console.log(min(2,3))
+// console.log(min(1,10))
+// console.log(min(10,1))
+// console.log(min(10000,10))
