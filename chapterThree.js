8dab41102bc7fcb5a6b47c088f4025123341c325
Eric-Andresen <erandre7@gmail.com>
2018-10-10 09:40:12 -0700

    Extend Ex. 3.1

--- chapterThree.js
+++ chapterThree.js
@@ -11,13 +11,30 @@ const sum = (array) => {
 
 const range = (startValue, endValue, step = 1) => {
     let result = [];
-    for (let i = startValue; i < endValue; i += step ){
-        result.push(i)
+    // check if ascending or descending
+    if ((startValue < endValue) & (Math.abs(step) == step)) {
+        for (let i = startValue; i < endValue; i += step ){
+            result.push(i)
+        }
+        return result;
+    } else if ((startValue > endValue) & (Math.abs(step) != step)) {
+        for (let i = startValue; i > endValue; i += step ){
+            result.push(i)
+        }
+        return result;
+    } else {
+        console.log("Error! Your Start and End values don't match your steps. It looks like you're stepping the wrong direction.")
     }
-    return result;
+    
 }
 
 // Tests
 // console.log(sum([1,1,1,1]))
 // console.log(range(2,15, 2))
 // console.log(sum(range(1,5)))
+console.log(range(5,2,-1))
+// => [5,4,3,2,1]
+console.log(range(1,10,2))
+// => [1,3,5,7,9]
+console.log(range(1,10,-2))
+// => Error!
