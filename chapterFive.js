03ac06213e70c12f66ae2bd0907c1d641efd93c7
Eric-Andresen <erandre7@gmail.com>
2018-10-17 09:21:04 -0700

    Ex 5.2 create loop function

--- chapterFive.js
+++ chapterFive.js
@@ -4,3 +4,18 @@ const flatten = (arrayOfArrays) => arrayOfArrays.reduce((a,b) => a.concat(b))
 
 flatten([[1,2],[3,4],[5,6]])
 // -> [ 1, 2, 3, 4, 5, 6 ]
+
+// Ex 2 | make loop() -> value -> test function (value -> Bool) -> update function -> body function 
+// if testFunction(value) then body function(value) else update(value)
+
+function loop(value, test, update, body) {
+    if (!test(value)){
+        return ;
+    } else {
+        body(value);
+        loop(update(value), test, update, body);
+    }
+}
+
+loop(3, n => n > 0, n => n - 1, console.log);
+// -> 3\n 2\n 1\n
