8e0972ee891c17eb9a2df19b4113ef20340caa78
Eric-Andresen <erandre7@gmail.com>
2018-10-17 09:30:27 -0700

    Ex 5.3 make every() returns if all elements of an array meet a condition

--- chapterFive.js
+++ chapterFive.js
@@ -2,7 +2,7 @@
 
 const flatten = (arrayOfArrays) => arrayOfArrays.reduce((a,b) => a.concat(b))
 
-flatten([[1,2],[3,4],[5,6]])
+// flatten([[1,2],[3,4],[5,6]])
 // -> [ 1, 2, 3, 4, 5, 6 ]
 
 // Ex 2 | make loop() -> value -> test function (value -> Bool) -> update function -> body function 
@@ -17,5 +17,14 @@ function loop(value, test, update, body) {
     }
 }
 
-loop(3, n => n > 0, n => n - 1, console.log);
+// loop(3, n => n > 0, n => n - 1, console.log);
 // -> 3\n 2\n 1\n
+
+// Ex 3 | Implement Array.every yourself
+
+const every = (array, condition) => array.reduce((a,b) => a && condition(b), true);
+
+console.log(every([1,2,3,4], x => x < 5))
+// -> true
+console.log(every([1,2,3,14], x => x < 5))
+// -> false
