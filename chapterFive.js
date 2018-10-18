b9db896942786f6067eaf1f002a69fcda3f743e9
Eric-Andresen <erandre7@gmail.com>
2018-10-18 11:10:48 -0700

    Ex 5.4 return of the script most of the writing it in.

--- chapterFive.js
+++ chapterFive.js
@@ -1,3 +1,5 @@
+require('./scripts.js');
+
 // Ex 1 | make flatten() -> array of arrays -> array with all elements of arrays
 
 const flatten = (arrayOfArrays) => arrayOfArrays.reduce((a,b) => a.concat(b))
@@ -24,7 +26,60 @@ function loop(value, test, update, body) {
 
 const every = (array, condition) => array.reduce((a,b) => a && condition(b), true);
 
-console.log(every([1,2,3,4], x => x < 5))
+// console.log(every([1,2,3,4], x => x < 5))
 // -> true
-console.log(every([1,2,3,14], x => x < 5))
+// console.log(every([1,2,3,14], x => x < 5))
 // -> false
+
+// Ex 4 | make dominantDirection() -> string -> direction of most of the writing
+// NOTE: uses objects SCRIPTS with language data related to unicode (see import above)
+
+function dominantDirection(string){
+    let directionCounts = {}
+    for (const c  of string){
+        const direction = getDirection(c)
+        
+        // increment value
+        if (directionCounts[direction]){
+            directionCounts[direction] += 1;
+        } else if (direction != null) {
+            directionCounts[direction] = 1;
+        }
+    }
+    return findMax(directionCounts)
+}
+
+// Helper Functions
+const getDirection = (char) => {
+    for (const s of SCRIPTS) {
+        if (in_range(char.codePointAt(0), s.ranges)){
+            return s.direction;
+        }
+    }
+    return null
+}
+
+const findMax = (dictionary) => {
+    let dominantEntry = 'No entry';
+    let highestValue = 0
+    for (const e of Object.entries(dictionary)){
+        const occurences = e[1];
+        const entry = e[0];
+        if (occurences > highestValue) {
+            dominantEntry = entry
+        }
+    }
+    return dominantEntry
+}
+
+function in_range(charCode, listOfRanges){
+    // check that code falls in one of the ranges 
+    const bool = listOfRanges.some( a => (charCode > a[0] && charCode < a[1]))
+    return bool
+}
+
+// Tests
+console.log(dominantDirection("Hello!"));
+// → ltr
+console.log(dominantDirection(" hey مساء الخير"));
+// → rtl
